1cc80f5ca0bee471c9ff711575db796782f40ae4
zhamal11 <zhamal.nurmanov@gmail.com>
2022-10-16 03:04:04 +0600

    add config

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 import "./styles/index.scss";
-import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {hovered: true, selected: false}, [theme, 'cls2'])}>
       <button onClick={toggleTheme}>toggle</button>
       <Link to={'/'}>Home</Link>
       <Link to={'/about'}>About</Link>
