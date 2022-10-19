1270ee2129d013f680017155310ccadafbb949e1
zhamal11 <zhamal.nurmanov@gmail.com>
2022-10-20 00:42:53 +0600

    third commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,19 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
 
 import "./styles/index.scss";
 import { classNames } from "helpers/classNames/classNames";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
 
   return (
     <div className={classNames('app', {hovered: true, selected: false}, [theme, 'cls2'])}>
+      <Navbar/>
+      <AppRouter/>
       <button onClick={toggleTheme}>toggle</button>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/about'}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            <Route path={'/about'} element={<AboutPage />} />
-            <Route path={'/'}  element={<MainPage />} />
-        </Routes>
-      </Suspense>
     </div>
   )
 }
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import { classNames } from 'helpers/classNames/classNames';
+import { Link } from 'react-router-dom';
+import AppLink from 'shared/ui/AppLink/AppLink';
+import cls from './Navbar.module.scss';
+
+interface NavbarProps {
+  className?: string;
+}
+
+export function Navbar({className}: NavbarProps) {
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink to={'/'} className={cls.mainLink}>Home</AppLink>
+        <AppLink to={'/about'}>About</AppLink>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
